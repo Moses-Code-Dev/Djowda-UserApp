56f014f1e61c590d8089de82c3011c9458c1abb7
Moses-Code-Dev <71468750+Moses-Code-Dev@users.noreply.github.com>
2025-10-19 18:34:43 +0100

    testing deployment workflow.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.djowda.djowdaUser"
         minSdk = 27
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
