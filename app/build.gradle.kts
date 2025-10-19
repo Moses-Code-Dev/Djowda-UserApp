0ef239065cc2d3b386d1b1f04c2afccdcd48744b
Moses-Code-Dev <71468750+Moses-Code-Dev@users.noreply.github.com>
2025-10-19 12:43:28 +0100

    testing deployment workflow.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.djowda.djowdaUser"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
