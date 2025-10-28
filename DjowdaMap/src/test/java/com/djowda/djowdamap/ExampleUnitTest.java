303bc03668266967f279994868d4cde275584de8
Moses-Code-Dev <71468750+Moses-Code-Dev@users.noreply.github.com>
2025-10-28 16:39:39 +0100

    code cleaning and initial flow setup.

--- DjowdaMap/src/test/java/com/djowda/djowdamap/ExampleUnitTest.java
+++ DjowdaMap/src/test/java/com/djowda/djowdamap/ExampleUnitTest.java
@@ -1,3 +1,33 @@
+/*
+ *
+ *  * Created by the Djowda Project Team
+ *  * Copyright (c) 2017-2025 Djowda. All rights reserved.
+ *  *
+ *  * This file is part of the Djowda Project.
+ *  *
+ *  * Licensed under the GNU Affero General Public License v3.0 (AGPL-3.0)
+ *  *
+ *  * You may redistribute and/or modify this file under the terms of the
+ *  * GNU Affero General Public License as published by the Free Software Foundation,
+ *  * either version 3 of the License, or (at your option) any later version.
+ *  *
+ *  * This file is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this file. If not, see <https://www.gnu.org/licenses/>.
+ *  *
+ *  * Notes:
+ *  * - The Djowda Project is a community-driven initiative evolving toward a decentralized food ecosystem.
+ *  * - Contributions are welcome under the AGPL terms.
+ *  *
+ *  * Last Modified: 2025-10-18 19:22
+ *
+ *
+ */
+
 package com.djowda.djowdamap;
 
 import org.junit.Test;
